1f6c77a3ccb07d12c965f658aa503f45e0841af4
ruksad <sidsid2810@gmail.com>
2022-03-29 09:33:39 +0530

    microservices

---
+++ elasticsearch/src/test/java/com/learn/scarycoders/elasticsearch/ElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.scarycoders.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
